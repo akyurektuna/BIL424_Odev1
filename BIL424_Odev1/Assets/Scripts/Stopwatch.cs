c8a72f2c2830e8761df06a55af0b2d5a193bdfb0
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2022-07-07 14:11:56 +0300

    added stopwatch and UI

---
+++ BIL424_Odev1/Assets/Scripts/Stopwatch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class Stopwatch : MonoBehaviour
+{
+    bool stopwatchActive = false;
+    float currentTime;
+    public TextMeshProUGUI currentTimeText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stopwatchActive == true)
+            currentTime = currentTime + Time.deltaTime;
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss\:fff");
+    }
+
+    public void StartStopwatch(){
+        stopwatchActive = true;
+    }
+
+    public void StopStopwatch(){
+        stopwatchActive = false;
+    }
+}
